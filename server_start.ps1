b356410b56c6a6568ced39486cbc24ef0a6b6782
uzuro <commit@uzuro.33mail.com>
2016-01-25 11:23:31 -0500

    Add change time zone

---
+++ server_start.ps1
@@ -0,0 +1,8 @@
+# Windows Server 2008 R2 Standard startup script
+# 1) Change Time zone
+# 2) Change computer name
+# 3) Enable RDP
+# 4) Run Windows Updates
+
+# 1) Change Time Zone
+tzutil.exe /s "Eastern Standard Time"
